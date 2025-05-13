6172b277deb635c7b240e6623a22719e903e906a
Win10 <Win10@LAB2OGR7>
2025-05-13 11:19:21 +0300

    Proje dosyası ekle.

---
+++ shinigamiZer0/Form1.Designer.cs
@@ -0,0 +1,407 @@
+﻿
+namespace shinigamiZer0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txturadi = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtyazaradi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.button6 = new System.Windows.Forms.Button();
+            this.txtkitapadi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbyazar = new System.Windows.Forms.ComboBox();
+            this.cmbtur = new System.Windows.Forms.ComboBox();
+            this.txtsayfasayisi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1144, 570);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.txturadi);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1136, 544);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tür ekle";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.txtyazaradi);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1136, 544);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "yazar ekle";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button7);
+            this.tabPage3.Controls.Add(this.textBox1);
+            this.tabPage3.Controls.Add(this.label7);
+            this.tabPage3.Controls.Add(this.label6);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.txtsayfasayisi);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.cmbtur);
+            this.tabPage3.Controls.Add(this.cmbyazar);
+            this.tabPage3.Controls.Add(this.button5);
+            this.tabPage3.Controls.Add(this.dataGridView3);
+            this.tabPage3.Controls.Add(this.button6);
+            this.tabPage3.Controls.Add(this.txtkitapadi);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1136, 544);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "kitap ekle";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "türadı :";
+            // 
+            // txturadi
+            // 
+            this.txturadi.Location = new System.Drawing.Point(89, 67);
+            this.txturadi.Name = "txturadi";
+            this.txturadi.Size = new System.Drawing.Size(100, 20);
+            this.txturadi.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(281, 6);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(849, 532);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "güncelle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(139, 167);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(276, 6);
+            this.dataGridView2.MultiSelect = false;
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(849, 532);
+            this.dataGridView2.TabIndex = 8;
+            this.dataGridView2.DoubleClick += new System.EventHandler(this.dataGridView2_DoubleClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(139, 138);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "ekle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txtyazaradi
+            // 
+            this.txtyazaradi.Location = new System.Drawing.Point(84, 67);
+            this.txtyazaradi.Name = "txtyazaradi";
+            this.txtyazaradi.Size = new System.Drawing.Size(100, 20);
+            this.txtyazaradi.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "yazaradı :";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(130, 289);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "güncelle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(276, 38);
+            this.dataGridView3.MultiSelect = false;
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView3.Size = new System.Drawing.Size(849, 500);
+            this.dataGridView3.TabIndex = 8;
+            this.dataGridView3.DoubleClick += new System.EventHandler(this.dataGridView3_DoubleClick);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(130, 260);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "ekle";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // txtkitapadi
+            // 
+            this.txtkitapadi.Location = new System.Drawing.Point(84, 54);
+            this.txtkitapadi.Name = "txtkitapadi";
+            this.txtkitapadi.Size = new System.Drawing.Size(100, 20);
+            this.txtkitapadi.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "kitap adı :";
+            // 
+            // cmbyazar
+            // 
+            this.cmbyazar.FormattingEnabled = true;
+            this.cmbyazar.Location = new System.Drawing.Point(84, 80);
+            this.cmbyazar.Name = "cmbyazar";
+            this.cmbyazar.Size = new System.Drawing.Size(121, 21);
+            this.cmbyazar.TabIndex = 10;
+            // 
+            // cmbtur
+            // 
+            this.cmbtur.FormattingEnabled = true;
+            this.cmbtur.Location = new System.Drawing.Point(84, 107);
+            this.cmbtur.Name = "cmbtur";
+            this.cmbtur.Size = new System.Drawing.Size(121, 21);
+            this.cmbtur.TabIndex = 11;
+            // 
+            // txtsayfasayisi
+            // 
+            this.txtsayfasayisi.Location = new System.Drawing.Point(84, 134);
+            this.txtsayfasayisi.Name = "txtsayfasayisi";
+            this.txtsayfasayisi.Size = new System.Drawing.Size(100, 20);
+            this.txtsayfasayisi.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "sayfa sayısı ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "yazar";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "tür";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(591, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 20);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(547, 15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "ara :";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(130, 318);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "sil";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1168, 594);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txturadi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtyazaradi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtsayfasayisi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbtur;
+        private System.Windows.Forms.ComboBox cmbyazar;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox txtkitapadi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ shinigamiZer0/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shinigamiZer0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        dbKutuphaneEntities db = new dbKutuphaneEntities();
+
+        private void Doldur() {
+
+            var tur = db.TblTurler.Select(x => new
+            {
+                id = x.id,
+                tur = x.turAdi
+
+            }).ToList();
+            cmbtur.DataSource = tur;
+            cmbtur.DisplayMember = "tur";
+            cmbtur.ValueMember = "id";
+            dataGridView1.DataSource = tur;
+
+            var yazar = db.TblYazarlar.Select(x => new
+            {
+                id = x.id,
+                yazar = x.yazarAdi
+            }).ToList();
+            cmbyazar.DataSource = yazar;
+            cmbyazar.DisplayMember = "yazar";
+            cmbyazar.ValueMember = "id";
+            dataGridView2.DataSource = yazar;
+
+            var kitap = db.TblKitaplar.Select(x => new
+            {
+                x.id,
+                x.kitapAdi,
+                x.TblTurler.turAdi,
+                x.TblYazarlar.yazarAdi,
+                x.sayfaSayisi
+            }).ToList();
+            dataGridView3.DataSource = kitap;
+        
+        
+        
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TblTurler tur = new TblTurler();
+            tur.turAdi = txturadi.Text;
+            db.TblTurler.Add(tur);
+            db.SaveChanges();
+            Doldur();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Doldur();
+        }
+        int turid,yazarid,kitapid;
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            turid = int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            var tur = db.TblTurler.Where(x => x.id == turid).FirstOrDefault();
+            txturadi.Text = tur.turAdi;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var guncelle = (from x in db.TblTurler where x.id == turid select x).FirstOrDefault();
+            guncelle.turAdi = txturadi.Text;
+            db.SaveChanges();
+            Doldur();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            TblYazarlar yazar = new TblYazarlar();
+            yazar.yazarAdi = txtyazaradi.Text;
+            db.TblYazarlar.Add(yazar);
+            db.SaveChanges();
+            Doldur();
+        }
+
+        private void dataGridView2_DoubleClick(object sender, EventArgs e)
+        {
+            yazarid = int.Parse(dataGridView2.CurrentRow.Cells[0].Value.ToString());
+            var yazar = db.TblYazarlar.Where(x => x.id == yazarid).FirstOrDefault();
+            txtyazaradi.Text = yazar.yazarAdi;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            TblKitaplar kitap = new TblKitaplar();
+            kitap.kitapAdi = txtkitapadi.Text;
+            kitap.turu = int.Parse(cmbtur.SelectedValue.ToString());
+            kitap.yazari = int.Parse(cmbyazar.SelectedValue.ToString());
+            kitap.sayfaSayisi = txtsayfasayisi.Text;
+            db.TblKitaplar.Add(kitap);
+            db.SaveChanges();
+            Doldur();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var sil = (from x in db.TblKitaplar where x.id == kitapid select x).FirstOrDefault();
+            db.TblKitaplar.Remove(sil);
+            db.SaveChanges();
+            Doldur();
+        }
+
+        private void dataGridView3_DoubleClick(object sender, EventArgs e)
+        {
+            kitapid = int.Parse(dataGridView3.CurrentRow.Cells[0].Value.ToString());
+            var kitap = db.TblKitaplar.Where(x => x.id == kitapid).FirstOrDefault();
+            txtkitapadi.Text = kitap.kitapAdi;
+            txtsayfasayisi.Text = kitap.sayfaSayisi;
+            cmbtur.Text = kitap.TblTurler.turAdi;
+            cmbyazar.Text = kitap.TblYazarlar.yazarAdi;
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var ara = from x in db.TblKitaplar
+                      where x.kitapAdi.Contains(textBox1.Text)
+                      select new
+                      {
+                          x.id,
+                          x.kitapAdi,
+                          x.TblTurler.turAdi,
+                          x.TblYazarlar.yazarAdi,
+                          x.sayfaSayisi
+
+
+                      };
+            dataGridView3.DataSource = ara.ToList();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var guncelle = (from x in db.TblKitaplar where x.id == kitapid select x).FirstOrDefault();
+            guncelle.kitapAdi = txtkitapadi.Text;
+            guncelle.turu = int.Parse(cmbtur.SelectedValue.ToString());
+            guncelle.yazari = int.Parse(cmbyazar.SelectedValue.ToString());
+            guncelle.sayfaSayisi = txtsayfasayisi.Text;
+            db.SaveChanges();
+            Doldur();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var guncelle = (from x in db.TblYazarlar where x.id == yazarid select x).FirstOrDefault();
+            guncelle.yazarAdi = txtyazaradi.Text;
+            db.SaveChanges();
+            Doldur();
+        }
+    }
+}
